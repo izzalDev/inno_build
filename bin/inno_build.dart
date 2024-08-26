c85be1d9c213d6fb150d9b94859712e18b4b611b
izzalDev <92254049+izzalDev@users.noreply.github.com>
2024-08-27 00:39:52 +0700

    chore: optimizing code

--- bin/inno_build.dart
+++ bin/inno_build.dart
@@ -1,47 +1,27 @@
 import 'dart:io';
 import 'package:args/args.dart';
 import 'package:cli_spin/cli_spin.dart';
+import 'package:inno_build/logic/app_logic.dart';
 import 'package:inno_build/models/build_mode.dart';
 import 'package:inno_build/services/app_id_service.dart';
 import 'package:inno_build/services/dependency_manager.dart';
 import 'package:inno_build/services/flutter_builder.dart';
 import 'package:inno_build/services/inno_setup_manager.dart';
-import 'package:inno_build/utils/config.dart';
 import 'package:inno_build/utils/constants.dart';
 import 'package:inno_build/utils/pubspec_manager.dart';
 
 Future<void> main(List<String> arguments) async {
-  late final BuildMode buildMode;
   final parser = ArgParser()
     ..addOption('app-id', abbr: 'a', help: 'Generate a new InnoSetup AppID.')
-    ..addFlag('release',
-        help: 'Generate the application in release mode (default).')
-    ..addFlag('debug',
-        abbr: 'd', help: 'Generate the application in debug mode.')
-    ..addFlag('install-inno',
-        abbr: 'i', help: 'Install Inno Setup if not present.')
+    ..addFlag('release', help: 'Generate the application in release mode (default).')
+    ..addFlag('debug', abbr: 'd', help: 'Generate the application in debug mode.')
+    ..addFlag('install-inno', abbr: 'i', help: 'Install Inno Setup if not present.')
     ..addFlag('verbose', abbr: 'v', help: 'Enable verbose output.')
     ..addFlag('quiet', abbr: 'q', help: 'Suppress output (quiet mode).')
     ..addFlag('help', abbr: 'h', help: 'Show this help message.')
     ..addFlag('version', abbr: 'v', help: 'Show version information.');
 
   final argResults = parser.parse(arguments);
-
-  // MARK: Validate debug and release flags
-  if (argResults['debug'] && argResults['release']) {
-    print('Error: --release and --debug cannot be used together.');
-    exit(64); // Exit code for usage error
-  } else if (argResults['debug']) {
-    buildMode = BuildMode.debug;
-  } else {
-    buildMode = BuildMode.release;
-  }
-
-  // MARK: Validate verbose and quiet flags
-  if (argResults['verbose'] && argResults['quiet']) {
-    print('Error: --verbose and --quiet cannot be used together.');
-    exit(64); // Exit code for usage error
-  }
 
   if (argResults['help'] as bool) {
     print(helpMessage);
@@ -55,110 +35,47 @@ Future<void> main(List<String> arguments) async {
 
   final verbose = argResults['verbose'] as bool;
   final quiet = argResults['quiet'] as bool;
+
+  if (verbose && quiet) {
+    print('Error: --verbose and --quiet cannot be used together.');
+    exit(64); // Exit code for usage error
+  }
+
+  final buildMode = _determineBuildMode(argResults);
+
   final pubspecManager = PubspecManager();
   final appIdService = AppIdService(pubspecManager);
   final dependencyManager = DependencyManager(verbose: verbose);
   final flutterBuilder = FlutterBuilder(buildMode);
-  final innoSetupManager =
-      InnoSetupManager(buildMode, verbose: verbose, quiet: quiet);
+  final innoSetupManager = InnoSetupManager(buildMode, verbose: verbose, quiet: quiet);
   final spinner = CliSpin();
 
-  // MARK: Handle App ID generation/update
-  if (argResults['app-id'] as bool) {
-    spinner.start('Generating new App ID...');
-    final newAppId = await appIdService.updateAppId();
-    spinner.success('Generated new App ID: $newAppId');
-  } else {
-    spinner.start('Checking App ID...');
-    final appId = await appIdService.ensureAppId();
-    spinner.success('Current App ID: $appId');
-  }
+  final appLogic = AppLogic(
+    argResults: argResults,
+    pubspecManager: pubspecManager,
+    appIdService: appIdService,
+    dependencyManager: dependencyManager,
+    flutterBuilder: flutterBuilder,
+    innoSetupManager: innoSetupManager,
+    spinner: spinner,
+    buildMode: buildMode,
+  );
 
-  // MARK: Check and Install Inno Setup
-  spinner.start('Checking Inno Setup...');
-  if (File(innoCompilerPath).existsSync()) {
-    spinner.success('Inno Setup is already installed.');
-  } else {
-    spinner.start('Downloading Inno Setup...');
-    try {
-      dependencyManager.ensureInnoSetupDownloaded();
-      spinner.success('Downloaded Inno Setup successfully.');
-    } catch (e) {
-      spinner.fail('Failed to download Inno Setup: $e');
-    }
-    spinner.start('Installing Inno Setup...');
-    try {
-      innoSetupManager.ensureInnoSetupInstalled();
-      spinner.success('Installed Inno Setup successfully.');
-    } catch (e) {
-      spinner.fail('Failed to install Inno Setup: $e');
-    }
-  }
-
-  // MARK: Build Flutter application
-  spinner.start('Building Flutter Windows application...');
   try {
-    if (verbose) {
-      spinner.stop();
-      await flutterBuilder.buildApp();
-      spinner.success('Built ${buildMode.buildPath}\\${Config.execName}.');
-    } else {
-      await flutterBuilder.buildApp();
-      spinner.success('Built ${buildMode.buildPath}\\${Config.execName}.');
-    }
+    await appLogic.run();
   } catch (e) {
-    spinner.fail('Failed to build Flutter application: $e');
+    print(e);
+    exit(64); // Exit code for usage error
   }
+}
 
-  // MARK: Install Inno Setup
-  if (argResults['install-inno'] as bool) {
-    if (File(innoSetupInstallerPath).existsSync()) {
-      spinner.start('Installing Inno Setup...');
-      try {
-        innoSetupManager.ensureInnoSetupInstalled();
-        spinner.success('Installed Inno Setup successfully.');
-      } catch (e) {
-        spinner.fail('Failed to install Inno Setup: $e');
-      }
-    } else {
-      spinner.start('Downloading Inno Setup...');
-      try {
-        dependencyManager.ensureInnoSetupDownloaded();
-        spinner.success('Downloaded Inno Setup successfully.');
-      } catch (e) {
-        spinner.fail('Failed to download Inno Setup: $e');
-      }
-      spinner.start('Installing Inno Setup...');
-      try {
-        innoSetupManager.ensureInnoSetupInstalled();
-        spinner.success('Installed Inno Setup successfully.');
-      } catch (e) {
-        spinner.fail('Failed to install Inno Setup: $e');
-      }
+BuildMode _determineBuildMode(ArgResults argResults) {
+  if (argResults['debug'] as bool) {
+    if (argResults['release'] as bool) {
+      print('Error: --release and --debug cannot be used together.');
+      exit(64); // Exit code for usage error
     }
+    return BuildMode.debug;
   }
-
-  // MARK: Build Inno Setup script
-  spinner.start('Building Inno Setup script...');
-  try {
-    File file = await innoSetupManager.buildInnoSetupScript();
-    spinner.success('Built ${file.path}.');
-  } catch (e) {
-    spinner.fail('Failed to build Inno Setup script: $e');
-  }
-
-  // MARK: Compile Inno Setup script
-  spinner.start('Compiling Inno Setup script...');
-  try {
-    if (verbose) {
-      spinner.stop();
-      await innoSetupManager.compileInnoSetupScript();
-      spinner.success('Compiled Inno Setup script successfully.');
-    } else {
-      await innoSetupManager.compileInnoSetupScript();
-      spinner.success('Compiled Inno Setup script successfully.');
-    }
-  } catch (e) {
-    spinner.fail('Failed to compile Inno Setup script: $e');
-  }
+  return BuildMode.release;
 }
--- lib/logic/app_logic.dart
+++ lib/logic/app_logic.dart
@@ -1,29 +1,139 @@
-// app_logic.dart
-
+import 'dart:io';
+import 'package:args/args.dart';
 import 'package:cli_spin/cli_spin.dart';
 import 'package:inno_build/models/build_mode.dart';
 import 'package:inno_build/services/app_id_service.dart';
 import 'package:inno_build/services/dependency_manager.dart';
 import 'package:inno_build/services/flutter_builder.dart';
 import 'package:inno_build/services/inno_setup_manager.dart';
-import 'package:inno_build/utils/iss_generator.dart';
+import 'package:inno_build/utils/config.dart';
+import 'package:inno_build/utils/constants.dart';
+import 'package:inno_build/utils/pubspec_manager.dart';
 
-Future<void> runAppLogic({
-  String? appId,
-  required BuildMode buildMode,
-  required bool verbose,
-}) async {
-  final spin = CliSpin();
+class AppLogic {
+  final ArgResults argResults;
+  final PubspecManager pubspecManager;
+  final AppIdService appIdService;
+  final DependencyManager dependencyManager;
+  final FlutterBuilder flutterBuilder;
+  final InnoSetupManager innoSetupManager;
+  final CliSpin spinner;
+  final BuildMode buildMode;
 
-  try {
-    // 1. Cek dan download vcredist jika diperlukan
-    // 2. Cek dan install Inno Setup jika diperlukan
-    // 3. Generate app_id jika belum ada atau diperintahkan oleh user
-    // 4. Build aplikasi Flutter untuk Windows
-    // 5. Build script Inno Setup
-    // 6. Compile Inno Setup script
-  } catch (e) {
-    spin.fail();
-    print('An error occurred during the process: $e');
+  AppLogic({
+    required this.argResults,
+    required this.pubspecManager,
+    required this.appIdService,
+    required this.dependencyManager,
+    required this.flutterBuilder,
+    required this.innoSetupManager,
+    required this.spinner,
+    required this.buildMode,
+  });
+
+  Future<void> run() async {
+    _validateFlags();
+    await _handleAppId();
+    await _checkAndInstallInnoSetup();
+    await _buildFlutterApp();
+    if (argResults['install-inno'] as bool) {
+      await _installInnoSetup();
+    }
+    await _buildAndCompileInnoSetupScript();
+  }
+
+  void _validateFlags() {
+    if (argResults['debug'] as bool && argResults['release'] as bool) {
+      throw ArgumentError('Error: --release and --debug cannot be used together.');
+    }
+    if (argResults['verbose'] as bool && argResults['quiet'] as bool) {
+      throw ArgumentError('Error: --verbose and --quiet cannot be used together.');
+    }
+  }
+
+  Future<void> _handleAppId() async {
+    if (argResults['app-id'] as bool) {
+      spinner.start('Generating new App ID...');
+      final newAppId = await appIdService.updateAppId();
+      spinner.success('Generated new App ID: $newAppId');
+    } else {
+      spinner.start('Checking App ID...');
+      final appId = await appIdService.ensureAppId();
+      spinner.success('Current App ID: $appId');
+    }
+  }
+
+  Future<void> _checkAndInstallInnoSetup() async {
+    spinner.start('Checking Inno Setup...');
+    if (File(innoCompilerPath).existsSync()) {
+      spinner.success('Inno Setup is already installed.');
+    } else {
+      await _downloadAndInstallInnoSetup();
+    }
+  }
+
+  Future<void> _downloadAndInstallInnoSetup() async {
+    spinner.start('Downloading Inno Setup...');
+    try {
+      await dependencyManager.ensureInnoSetupDownloaded();
+      spinner.success('Downloaded Inno Setup successfully.');
+    } catch (e) {
+      spinner.fail('Failed to download Inno Setup');
+      stderr.writeln(e);
+    }
+    spinner.start('Installing Inno Setup...');
+    try {
+      await innoSetupManager.ensureInnoSetupInstalled();
+      spinner.success('Installed Inno Setup successfully.');
+    } catch (e) {
+      spinner.fail('Failed to install Inno Setup');
+      stderr.writeln(e);
+    }
+  }
+
+  Future<void> _buildFlutterApp() async {
+    spinner.start('Building Flutter Windows application...');
+    try {
+      await flutterBuilder.buildApp();
+      spinner.success('Built ${buildMode.buildPath}\\${Config.execName}.');
+    } catch (e) {
+      spinner.fail('Failed to build Flutter application');
+      stderr.writeln(e);
+    }
+  }
+
+  Future<void> _installInnoSetup() async {
+    if (File(innoSetupInstallerPath).existsSync()) {
+      spinner.start('Installing Inno Setup...');
+      try {
+        await innoSetupManager.ensureInnoSetupInstalled();
+        spinner.success('Installed Inno Setup successfully.');
+      } catch (e) {
+        spinner.fail('Failed to install Inno Setup');
+        stderr.writeln(e);
+      }
+    } else {
+      await _downloadAndInstallInnoSetup();
+    }
+  }
+
+  Future<void> _buildAndCompileInnoSetupScript() async {
+    spinner.start('Building Inno Setup script...');
+    try {
+      final file = await innoSetupManager.buildInnoSetupScript();
+      spinner.success('Built ${file.path}.');
+    } catch (e) {
+      spinner.fail('Failed to build Inno Setup script');
+      stderr.writeln(e);
+    }
+
+    spinner.start('Compiling Inno Setup script...');
+    try {
+      await innoSetupManager.compileInnoSetupScript();
+      spinner.success('Compiled Inno Setup script successfully.');
+    } catch (e) {
+      spinner.fail('Failed to compile Inno Setup script');
+      stderr.writeln(e);
+    }
   }
 }
