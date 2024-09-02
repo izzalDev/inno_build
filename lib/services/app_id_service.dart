59d880d6e60376d82ccf923034606108cf1720ff
izzalDev <92254049+izzalDev@users.noreply.github.com>
2024-09-02 15:57:20 +0700

    chore: add missing documentation

--- lib/services/app_id_service.dart
+++ lib/services/app_id_service.dart
@@ -3,11 +3,27 @@
 import 'package:inno_build/utils/pubspec_manager.dart';
 import 'package:uuid/uuid.dart';
 
+/// Class that handles the app id
+///
+/// The app id is a unique identifier that is used to identify the app in the
+/// Windows registry. It is automatically generated when the user runs `inno
+/// build` for the first time with the `--app-id` flag.
+///
+/// The app id is stored in the `pubspec.yaml` file under the key
+/// `inno_build.app_id`.
 class AppIdService {
+  /// The pubspec manager used to read and write the `pubspec.yaml` file.
   final PubspecManager _pubspecManager;
 
+  /// Creates a new instance of [AppIdService].
   AppIdService(this._pubspecManager);
 
+  /// Ensures that the app id exists in the `pubspec.yaml` file.
+  ///
+  /// If the app id does not exist, it is generated and saved to the
+  /// `pubspec.yaml` file. If it exists, the existing value is returned.
+  ///
+  /// Returns the app id.
   Future<String> ensureAppId() async {
     final appId = Uuid().v4();
     final pubspec = _pubspecManager.load();
@@ -21,6 +37,11 @@ class AppIdService {
     }
   }
 
+  /// Updates the app id in the `pubspec.yaml` file.
+  ///
+  /// Generates a new app id and saves it to the `pubspec.yaml` file.
+  ///
+  /// Returns the new app id.
   Future<String> updateAppId() async {
     final appId = Uuid().v4();
     final pubspec = _pubspecManager.load();
